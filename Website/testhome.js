dc70d6a3bb7ff95828b08a95742b81112a0ce320
ChawinNg <ik.chawin@gmail.com>
2023-05-19 16:49:19 +0700

    nothing

--- Website/testhome.js
+++ Website/testhome.js
@@ -36,3 +36,5 @@ var delayInMilliseconds = 1000; //1 second
 setTimeout(function() {
   //your code to be executed after 1 second
 }, delayInMilliseconds);
+
+document.body.style.backgroundImage = "url('bgPlant.jpg')";

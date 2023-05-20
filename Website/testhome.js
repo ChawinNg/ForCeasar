a2784f0c3f7f8de5a2c54fb8c015ca7865468553
WinSukon <jakrinsukon@gmail.com>
2023-05-20 16:40:51 +0700

    add hover information on home page

--- Website/testhome.js
+++ Website/testhome.js
@@ -1,40 +1,7 @@
-// Load the Visualization API and the corechart package.
-google.charts.load('current', {'packages':['corechart']});
-
-// Set a callback to run when the Google Visualization API is loaded.
-
-google.charts.setOnLoadCallback(drawChart);
-
-// Callback that creates and populates a data table,
-// instantiates the pie chart, passes in the data and
-// draws it.
-var data_array=[
-      ['Year', 'Sales', 'Expenses'],
-      ['2004',  1000,      400],
-      ['2005',  1170,      460],
-      ['2006',  660,       1120],
-      ['2007',  1030,      540]
-    ]
-
-var options = {
-    title: 'HUMIDITY vs TIME',
-    curveType: 'function',
-    legend: { position: 'bottom' }
-};
-function drawChart() {
-    data_array.push([(parseInt(data_array[data_array.length-1][0])+1).toString(),data_array[data_array.length-1][1]+1,data_array[data_array.length-1][2]+1])
-    var data = google.visualization.arrayToDataTable(data_array);
-    var chart = new google.visualization.LineChart(document.getElementById('curve_chart'));
-    var chart2 = new google.visualization.PieChart(document.getElementById('myChart'));
-    //chart.draw(data, options);
-    chart.draw(data, options);
-}
-
-
-var delayInMilliseconds = 1000; //1 second
-
-setTimeout(function() {
-  //your code to be executed after 1 second
-}, delayInMilliseconds);
 
 document.body.style.backgroundImage = "url('bgPlant.jpg')";
+
+function myFunction() {
+  var popup = document.getElementById("myPopup");
+  popup.classList.toggle("show");
+}

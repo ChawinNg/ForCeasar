dda3c09d92fbd45e73be754240d21f56ca89265e
ChawinNg <ik.chawin@gmail.com>
2023-05-22 13:52:59 +0700

    script_percent

--- Website/testhome.js
+++ Website/testhome.js
@@ -2,6 +2,19 @@
 document.body.style.backgroundImage = "url('bgPlant.jpg')";
 
 function myFunction() {
-  var popup = document.getElementById("myPopup");
+  var popup = document.getElementById("myPopup") ;
   popup.classList.toggle("show");
 }
+
+function getValue( ){
+    for(var i=0;i<101;i++){
+      return i;
+    }
+}
+
+function circleColor(){
+  var x = getValue();
+  document.getElementById("num").innerHTML= x;
+}
+
+setInterval(circleColor(),1000);
